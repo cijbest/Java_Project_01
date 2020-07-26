94ce7926ecc12c71db8859299f5d98bf332fadb2
cijbest <cijbest@gmail.com>
2020-07-26 18:10:50 +0900

    #Lotto_Game_Project

---
+++ Ws2.java
@@ -0,0 +1,66 @@
+package forwhile;
+/*****************************************************
+ * Number Guess Game
+ * 1. 프로그램을 실행한다.
+ * 2. 1~100 사이에 의의 정수가 자동 생성된다.
+ * 3. 사용자는 1~100까지의 숫자를 입력한다.
+ * 4. 입력한 값이 범위르 벗어나거나 문자가 입력되면 다시 입력 하라고 나온다.
+ * 5. 입력한 숫자가 생성된 수보다 크면 크다
+ *    작으면 작다라고 표시한다.
+ * 6. 맞으면 정답이라고 표시한다.
+ * 7. 진행 한 횟수를 표시한다.
+ * 8. 정답을 맞추면 다시 시작을 눌러 게임을 다시 시작 한다.
+ * 9. 정답을 맞춘 횟수를 표시 한다.
+ * 
+ * String str = "A"; char c = 'A'; char c2 = str.charAt(0); // c1 == c2 비교가 가능
+ * ****************************************************/
+ 
+import java.util.Random;
+import java.util.Scanner;
+ 
+public class Ws3 {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        
+        System.out.println("===============================");
+        System.out.println("Game Start : s | Quit Game : q");
+        System.out.println("===============================");
+        while(true) {
+            
+            System.out.print("Enter the key : ");
+            String input = sc.nextLine();
+ 
+            if(input.equals("s") || input.equals("r")) {
+                int correct = r.nextInt(100) + 1;
+                int count = 0;
+                while(true) {
+                    System.out.print("Input Number : ");
+                    int n = Integer.parseInt(sc.nextLine());
+                    if(n < 1 || n > 100) {
+                        System.out.println("Try Again!!");
+                        continue;
+                    }
+                    count++;
+                    if(n > correct) {
+                        System.out.println("-> Number is too Big!!");
+                    } else if(n < correct) {
+                        System.out.println("-> Number is too Small!!");
+                    } else {
+                        System.out.printf("!!! Correct!!! You move %d Times!!\n", count);
+                        System.out.println("=============================");
+                        System.out.println("Restart : r | Quit Game : q");
+                        System.out.println("=============================");
+                        break;    
+                    }
+                }
+            }else if(input.equals("q")) {
+                sc.close();
+                break;    
+            }else {
+                System.out.println("Please Enter the key again... ");
+            }
+        }
+    }
+}
